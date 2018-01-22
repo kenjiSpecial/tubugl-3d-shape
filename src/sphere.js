f6b12e86107bed03fafaf24588c2a7d9e1e5bdee
Kenji Saito <k.saito.1985@gmail.com>
2018-01-22 17:04:43 +0100

    Update sphere.js

--- src/sphere.js
+++ src/sphere.js
@@ -140,7 +140,7 @@ export class Sphere extends Object3D {
 		else this._gl.disable(DEPTH_TEST);
 
 		if (this._isTransparent) {
-			this.gl.blendFunc(SRC_ALPHA, ONE_MINUS_SRC_ALPHA);
+			this._gl.blendFunc(SRC_ALPHA, ONE_MINUS_SRC_ALPHA);
 			this._gl.enable(BLEND);
 		} else {
 			this._gl.blendFunc(ONE, ZERO);
